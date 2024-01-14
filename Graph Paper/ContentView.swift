b24f17110fbc22fea612d6778e73d62bbd5df118
Livsy90 <artmir@live.ru>
2024-01-14 12:39:21 +0400

    Initial Commit

---
+++ Graph Paper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Graph Paper
+//
+//  Created by Livsy on 14.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Graph Paper/Graph_PaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Graph_PaperApp.swift
+//  Graph Paper
+//
+//  Created by Livsy on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Graph_PaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
